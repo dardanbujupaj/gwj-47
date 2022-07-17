dcf7576d3f62ff9d782b2f6e02a514808096ae5f
Dardan Bujupaj <dardan@brudi.com>
2022-07-17 16:56:34 +0200

    fix: replace references to audiostreamplayer with musicengine (#13)

--- scenes/Level_2.gd
+++ scenes/Level_2.gd
@@ -21,7 +21,6 @@ func _ready() -> void:
 func _unhandled_input(event):
 	if event.is_action("switch") and event.is_pressed():
 		switch_world()
-	
 
 
 func switch_world() -> void:
@@ -29,18 +28,10 @@ func switch_world() -> void:
 	
 	down = !down
 	
-	if $LowerWorld/Character != manifested_character:
-		tween.interpolate_property($LowerWorld/AudioStreamPlayer, "volume_db", $LowerWorld/AudioStreamPlayer.volume_db, linear2db(1.0 if down else 0.2), 0.5)
-		$LowerWorld/Character.active = down
-	if $UpperWorld/Character != manifested_character:
-		tween.interpolate_property($UpperWorld/AudioStreamPlayer, "volume_db", $UpperWorld/AudioStreamPlayer.volume_db, linear2db(0.2 if down else 1.0), 0.5)
-		$UpperWorld/Character.active = !down
+	if down:
+		MusicEngine.play_lower()
+	else:
+		MusicEngine.play_upper()
 	
-	var screen = Vector2(1920, 1080)
-	
-	#var camera_pos = screen / 2 if down else screen / 2 * Vector2(1, -1)
-	#tween.interpolate_property($Camera2D, "position", $Camera2D.position, camera_pos, 0.5, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	
-	#tween.start()
-	
-	
+	$LowerWorld/Character.active = down
+	$UpperWorld/Character.active = !down
